45288082b89068b161d779dc477b9d50aa14625f
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2020-04-20 16:42:44 +0200

    Modul 3: Enumeratoren und Methoden (Kommentiert)

---
+++ Enumeratoren/Module1.vb
@@ -0,0 +1,53 @@
+﻿Module Module1
+
+    'ENUMERATOREN sind Datentypen, deren Variablen ausschließlich bestimmte vordefinierte Werte annehmen können.
+    ''Enumeratoren müssen außerhalb von Methoden definiert werden
+    Enum Wochentag
+        Montag = 1
+        Dienstag
+        Mittwoch
+        Donnerstag
+        Freitag
+        Samstag
+        Sonntag
+    End Enum
+
+
+    Sub Main()
+
+        'Deklaration und Initialisierung einer Enumerator-Variablen
+        Dim heute As Wochentag = Wochentag.Montag
+        Console.WriteLine($"Heute ist ein {heute}.")
+
+        'Durchlauf über die möglichen Zustande
+        Console.WriteLine("Welcher Wochentag ist heute?")
+        For index = 1 To 7
+            Console.WriteLine($"{index}: {DirectCast(index, Wochentag)}")
+        Next
+
+        'Abfrage eines Enumerator-Zustandes vom Benutzer und Umwandlung von String -> Int -> Enumarator
+        heute = Integer.Parse(Console.ReadLine())
+        Console.WriteLine($"Heute ist ein {heute}.")
+
+
+        'SELECTs sind verkürzte If-Blöcke, mit denen genau eine Variable auf ihren Zustand hin überprüft wird.
+        ''Jeder Zustand, bei dem etwas passieren soll, wird als CASE definiert, in welchem dann der spezifische Programm-
+        ''verlauf definiert wird. Wenn die Variable einen nicht näher definierten Zustand annimmt, wird der ELSE-CASE
+        ''ausgeführt
+        Select Case heute
+            Case Wochentag.Montag
+                Console.WriteLine("Schönen Wochenstart")
+            Case Wochentag.Mittwoch
+                Console.WriteLine("Hälfte geschafft")
+            Case Wochentag.Freitag
+                Console.WriteLine("Woche ist fast vorbei...")
+            Case Else
+                Console.WriteLine("Woche läuft...")
+        End Select
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ Methoden/Module1.vb
@@ -0,0 +1,58 @@
+﻿Module Module1
+
+    'Methoden sind Programmteile, von denen jeder eine sehr spezielle und kleinteilige Aufgabe ausführt.
+    ''Es wird unterschieden zwischen Prozeduren, welche keinen Rückgabewert besitzen und mit dem Stichwort
+    ''SUB gekennzeichnet sind...
+    Sub AddiereUndGibAus(a As Integer, b As Integer)
+        Dim summe = a + b
+        Console.WriteLine(summe)
+    End Sub
+
+    '...und Funktionen, die einen Rückgabewert besitzen und mit dem Stichwort FUNCTION markiert werden.
+    ''Jede Methode besteht aus einem Kopf (einer Signatur) in welchem der Zugriff und die Methodenart
+    ''definiert werden, ein Bezeichner für die Methode bestimmt wird und Übergabeparameter (und Rückgabe-
+    ''wert bei Funktionen) definiert werden... 
+    Function Addiere(a As Integer, b As Integer) As Integer
+        '...und einem Körper, in welchem die Programmlogik hinterlegt wird.
+        ''Mittels des RETURN-Befehls wird in Funktionen der Rückgabewert an den Aufrufer gesand und die
+        ''Ausführung der Methode unterbrochen
+        Dim summe = a + b
+        Return summe
+    End Function
+
+    'Methoden, welche den gleichen Bezeichner haben aber unterschiedliche Übergabeparameter heißen 'ÜBERLADENE
+    ''Methoden'. Eine Eindeutigkeit wird durch die verschiedenen Übergabeparameter erzielt. 
+    Function Addiere(a As Double, b As Double) As Double
+        Dim summe = a + b
+        Return summe
+    End Function
+
+    'Mit dem OPTIONAL-Stichwort kann man Default-Werte für die Übergabeparameter definieren. Diese werden
+    ''bei Nicht-Angabe der Parameter während des Methodenaufrufs eingesetzt.
+    Function Addiere(a As Integer, b As Integer, c As Integer, Optional d As Integer = 0) As Integer
+        Dim summe = a + b + c + d
+        Return summe
+    End Function
+
+    Sub Main()
+
+        'Prozeduraufruf
+        AddiereUndGibAus(45, 78)
+        AddiereUndGibAus(23, 78)
+        AddiereUndGibAus(4556, 78)
+        AddiereUndGibAus(-87, 78)
+
+        'Funktionsaufruf (Addiere(int,int))
+        Dim erg As Integer = Addiere(12, 89)
+
+        'Funktionsaufruf (Addiere(double,double))
+        Dim dblErg As Double = Addiere(12.3, 45.8)
+
+        'Funktionsaufruf (Addiere mit optionalem Parameter)
+        erg = Addiere(12, 12, 45)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
